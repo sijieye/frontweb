e881bc4db909d8596a2f08adaeb257514ba02bac
sijieye <sijieye@bu.edu>
2023-04-18 22:49:45 -0400

    Azure deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.listen(port, () => {
+    console.log(`Server is running on port 3001.`);
+  });
